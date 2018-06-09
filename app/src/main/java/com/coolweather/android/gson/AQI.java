c81e06dd6765205a30d0854af4a5803fc53223b3
phb <249570608@qq.com>
2018-06-09 18:17:42 +0800

    基本功能实现

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2018/6/9
+ */
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
